af04b5390ee14eac585f3f3ca75bbfbabc1f45c8
kosti <koistinen@gmail.com>
2026-04-10 15:07:33 +0300

    Tehty testejä jUnitilla, käyty varoitukset ja ilmoitukset läpi (pari suppressia) ja muuten IDE:n ehdottamia korjauksia. Poistettu turhia FXML-palikoita koodista, joilla ei tehdä koodissa mitään.

---
+++ src/test/java/KategoriaServiceTest.java
@@ -0,0 +1,74 @@
+import fi.jyu.ohj2.kosti.kulujenSeuranta.model.Kategoria;
+import fi.jyu.ohj2.kosti.kulujenSeuranta.model.Tapahtuma;
+import fi.jyu.ohj2.kosti.kulujenSeuranta.model.Tyyppi;
+import fi.jyu.ohj2.kosti.kulujenSeuranta.service.KategoriaService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KategoriaServiceTest {
+
+
+    /// Tyhjennetään lista ennen testausta.
+    @BeforeEach
+    void tyhjenna(){
+        KategoriaService.getKategoriat().clear();
+    }
+
+    ///  Testataan kategorian lisääminen.
+    @Test
+    void kategorianLisaysToimii(){
+        KategoriaService.lisaaKategoria(new Kategoria("testi", Tyyppi.MENO));
+        KategoriaService.lisaaKategoria(new Kategoria("testi2", Tyyppi.TULO));
+
+        assertEquals(2, KategoriaService.getKategoriat().size());
+        assertEquals("testi", KategoriaService.getKategoriat().get(0).getNimi());
+        assertEquals("testi2", KategoriaService.getKategoriat().get(1).getNimi());
+        assertEquals(Tyyppi.MENO, KategoriaService.getKategoriat().get(0).getTyyppi());
+        assertEquals(Tyyppi.TULO, KategoriaService.getKategoriat().get(1).getTyyppi());
+    }
+
+    ///  Testataan kategorian poistoa.
+    @Test
+    void kategorianPoistoToimii() {
+        Kategoria testi1 = new Kategoria("testi1", Tyyppi.MENO);
+        Kategoria testi2 = new Kategoria("testi2", Tyyppi.TULO);
+
+        KategoriaService.lisaaKategoria(testi1);
+        KategoriaService.lisaaKategoria(testi2);
+
+        ObservableList<Tapahtuma> tapahtuma = FXCollections.observableArrayList();
+
+        KategoriaService.poistaKategoria(testi1, tapahtuma);
+
+        assertEquals(1, KategoriaService.getKategoriat().size());
+        assertEquals("testi2", KategoriaService.getKategoriat().getFirst().getNimi());
+
+        KategoriaService.poistaKategoria(testi2, tapahtuma);
+
+        assertEquals(0, KategoriaService.getKategoriat().size());
+        assertFalse(KategoriaService.getKategoriat().contains(testi1));
+        assertFalse(KategoriaService.getKategoriat().contains(testi2));
+    }
+
+    ///  Testataan saman nimisten kategorioiden poistoa, joissa eri tyyppi.
+    @Test
+    void kategorianPoistoTyypinMukaanToimii(){
+        Kategoria testi1 = new Kategoria("testi", Tyyppi.MENO);
+        Kategoria testi2 = new Kategoria("testi", Tyyppi.TULO);
+
+        KategoriaService.lisaaKategoria(testi1);
+        KategoriaService.lisaaKategoria(testi2);
+
+        ObservableList<Tapahtuma> tapahtuma = FXCollections.observableArrayList();
+
+        KategoriaService.poistaKategoria(testi1, tapahtuma);
+
+        assertEquals(1, KategoriaService.getKategoriat().size());
+        assertEquals("testi", KategoriaService.getKategoriat().getFirst().getNimi());
+        assertEquals(Tyyppi.TULO, KategoriaService.getKategoriat().getFirst().getTyyppi());
+    }
+}
---
+++ src/test/java/TapahtumaTest.java
@@ -0,0 +1,52 @@
+import fi.jyu.ohj2.kosti.kulujenSeuranta.model.Kategoria;
+import fi.jyu.ohj2.kosti.kulujenSeuranta.model.Tapahtuma;
+import fi.jyu.ohj2.kosti.kulujenSeuranta.model.Tyyppi;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+
+
+public class TapahtumaTest {
+
+    ///  Testataan gettereiden toiminta
+    @Test
+    void getNimiToimii() {
+        Tapahtuma tapahtuma = new Tapahtuma();
+        tapahtuma.setAihe("kissa");
+        assertEquals("kissa", tapahtuma.getAihe());
+    }
+    @Test
+    void getSummaToimii() {
+        Tapahtuma tapahtuma = new Tapahtuma();
+        tapahtuma.setSumma(100);
+        assertEquals(100, tapahtuma.getSumma());
+    }
+    @Test
+    void getPvmToimii() {
+        Tapahtuma tapahtuma = new Tapahtuma();
+        tapahtuma.setPvm(LocalDate.of(2026, 4, 9));
+        assertEquals(LocalDate.of(2026, 4, 9), tapahtuma.getPvm());
+    }
+
+
+    /// Testataan Tapahtuma-olion konstruktorin toiminta. Testissä tehdään myös yksi uusi kategoria,
+    /// jotta testi voidaan tehdä.
+    @Test
+    void konstruktoriToimii() {
+        LocalDate pvm = LocalDate.of(2026, 4, 9);
+        Kategoria kategoria = new Kategoria("testi", Tyyppi.MENO);
+        Tapahtuma tapahtuma = new Tapahtuma(pvm, "testi", kategoria, 100, true, Tyyppi.MENO);
+
+        assertEquals(pvm, tapahtuma.getPvm());
+        assertEquals("testi", tapahtuma.getAihe());
+        assertEquals(kategoria, tapahtuma.getKategoria());
+        assertEquals(100, tapahtuma.getSumma());
+        assertTrue(tapahtuma.isPakollinen());
+        assertEquals(Tyyppi.MENO, tapahtuma.getTyyppi());
+
+    }
+
+
+
+
+}
